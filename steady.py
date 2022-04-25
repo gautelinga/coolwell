5dc7cb83c8b19a192e2e16b44b354b8672a716eb
Beatrice Baldelli <42637448+Beatriba@users.noreply.github.com>
2022-04-25 17:09:21 +0200

    Update steady.py

--- steady.py
+++ steady.py
@@ -1,7 +1,8 @@
 import dolfin as df
 import argparse
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
+import math
 from mesh_box import square_cavity_mesh
 import os
 from bcs import PeriodicBC, Wall, TopWall, BtmWall, NotWall
@@ -57,7 +58,7 @@ coords = mesh.coordinates()[:]
 
 Eu = df.VectorElement("Lagrange", mesh.ufl_cell(), 2)
 Ep = df.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-ET = df.FiniteElement("Lagrange", mesh.ufl_cell(), 2)
+ET = df.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 
 pbc = PeriodicBC(Lx, b, h)
 wall = Wall(Lx, b, h)
@@ -134,16 +135,24 @@ solver.parameters["newton_solver"]["krylov_solver"]["nonzero_initial_guess"] = T
 
 solver.solve()
 
-one = df.interpolate(df.Constant(1.), W.sub(2).collapse())
-V_Omega = df.assemble(one*df.dx)
+one_well = df.interpolate(df.Expression('x[1] < DOLFIN_EPS ? 1. : 0.', degree=0), W.sub(2).collapse())
+V_Omega_well = df.assemble(one_well*df.dx)
+one_channel = df.interpolate(df.Expression('x[1] < DOLFIN_EPS ? 0. : 1.', degree=0), W.sub(2).collapse()) 
+V_Omega_channel = df.assemble(one_channel*df.dx)
 
-ux_mean = df.assemble(u_[0]*df.dx)/V_Omega
+u_b = df.assemble(df.sqrt(df.dot(u_,u_))*one_well*df.dx)/V_Omega_well
+u_0 = df.assemble(df.sqrt(df.dot(u_,u_))*one_channel*df.dx)/V_Omega_channel
 
 if rank == 0:
-    print("ux_mean = {}".format(ux_mean))
+    print("u_b = {}".format(u_b))
+    print("u_0 = {}".format(u_0))
+    if u_b != 0:
+       print("u_0/u_b = {}".format(u_0/u_b))
+   
 
 nu_mean = (args.nutop+args.nubtm)*0.5
-Re = args.b*ux_mean/nu_mean
+Re = args.b*u_b/nu_mean
+Re_0 = args.b*u_0/nu_mean
 Pr = nu_mean/args.kappa
 Pe = Re*Pr
 Ra = args.alpha*(args.Ttop-args.Tbtm)*b**3/(nu_mean*args.kappa)
@@ -154,27 +163,29 @@ if args.fx != 0:
 ds = df.Measure("ds", domain=mesh, subdomain_data=subd)
 
 qy = df.assemble(kappa*T_.dx(1)*ds(3))
-qy_ref = args.kappa * (args.Ttop-args.Tbtm) / (args.Lx+args.H) * args.Lx
+qy_ref = args.kappa * (args.Ttop-args.Tbtm) / (args.b+args.H) * args.b
 Nu = qy/qy_ref
 
 if rank == 0:
     print("Re = {}".format(Re))
+    print("Re_0 = {}".format(Re_0))
     print("Ra = {}".format(Ra))
     print("Pe = {}".format(Pe))
     print("RT = {}".format(RT))
     print("Nu = {}".format(Nu))
+    print("qy = {}".format(qy))
 
 U_, P_, To_ = w_.split(deepcopy=True)
 U_.rename("u", "tmp")
-#U_.vector()[:] /= ux_mean
+#U_.vector()[:] /= u_b
 To_.rename("T", "tmp")
 
-subfolder = "{}/Re{}_b{}".format(folder, Re, args.b)
-if rank == 0 and not os.path.exists(folder):
+subfolder = "{}/RT{:.7f}_Re{:.5f}_Pe{:.5f}_b{}_H{}".format(folder, RT, Re, Pe, args.b, args.H)
+if rank == 0 and not os.path.exists(subfolder):
     os.makedirs(subfolder)
 
 with df.HDF5File(mesh.mpi_comm(),
-                 "{}/flow.xdmf".format(subfolder), "w") as h5f:
+                 "{}/flow.h5".format(subfolder), "w") as h5f:
     h5f.write(w_, "w")
 
 with df.XDMFFile(mesh.mpi_comm(),
@@ -184,3 +195,9 @@ with df.XDMFFile(mesh.mpi_comm(),
 with df.XDMFFile(mesh.mpi_comm(),
                  "{}/T.xdmf".format(subfolder)) as xdmff:
     xdmff.write(To_)
+
+if rank == 0:
+   with open("{}/parameters.dat".format(subfolder),"w") as file:
+      file.write("u_b = {}\nu_0 = {}\nRe_b = {}\nR_0 = {}\nRa = {}\nPe = {}\nRT = {}\nNu = {}\nqy = {}".format(u_b,u_0,Re,Re_0,Ra,Pe,RT,Nu,qy))
+      file.write("\nmpiexec -n 4 python3 steady.py -res {} -Lx {} -b {} -H {} -fx {} -Ttop {} -Tbtm {} -alpha {} -nutop {} -nubtm {} -kappa {}".format(args.res, args.Lx, args.b, args.H, args.fx, args.Ttop, args.Tbtm, args.alpha, args.nutop, args.nubtm, args.kappa))
+   
